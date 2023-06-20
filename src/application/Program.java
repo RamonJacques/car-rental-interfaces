0529c5680d2cd50437476bcfcba9c6068c91e15d
RamonJacques <ramonjacquesrs@gmail.com>
2023-06-20 14:57:35 -0300

    Entities created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,35 @@
 package application;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
+		DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		
+		System.out.println("Entre with rent data.");
+		System.out.print("Car model: ");
+		String carModel = sc.nextLine();
+		System.out.print("Start (dd/MM/yyyy hh:mm): ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(),dtf1);
+		System.out.print("Finish (dd/MM/yyyy hh:mm): ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(),dtf1);
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+				
+		
+		sc.close();
 	}
 
 }
